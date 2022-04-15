7f2c56e4f7556001d19e59e850d20600a68b9012
Bertrand Keller <bertrand.keller@gmail.com>
2022-04-15 15:10:13 +0200

    delete static

--- _static/vercel.sh
+++
@@ -1,9 +0,0 @@
-if [[ "$VERCEL_GIT_COMMIT_REF" == "$BRANCH_TO_BUILD" ]] ; then
-  # Proceed with the build
-    echo "✅ - Build can proceed"
-  exit 1;
-else
-  # Don't build
-  echo "🛑 - Build canceled"
-  exit 0;
-fi
